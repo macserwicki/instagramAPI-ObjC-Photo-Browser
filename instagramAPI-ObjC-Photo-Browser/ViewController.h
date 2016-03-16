ee99624dc1a4de2a3561398b7b818f3cb3946613
Maciej Serwicki <macserwicki@gmail.com>
2016-03-15 21:56:24 -0500

    first commit

---
+++ instagramAPI-ObjC-Photo-Browser/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  instagramAPI-ObjC-Photo-Browser
+//
+//  Created by Maciej Serwicki on 3/15/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) NSString *testProperty;
+
+@end
+
---
+++ instagramAPI-ObjC-Photo-Browser/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  instagramAPI-ObjC-Photo-Browser
+//
+//  Created by Maciej Serwicki on 3/15/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
