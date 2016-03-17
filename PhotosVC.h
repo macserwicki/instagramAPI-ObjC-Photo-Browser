7188e52ab726e5f8654563d4a4f444559295902b
Maciej Serwicki <macserwicki@gmail.com>
2016-03-16 22:37:50 -0500

    Works Without Storyboard

---
+++ PhotosVC.h
@@ -0,0 +1,13 @@
+//
+//  PhotosVC.h
+//  instagramAPI-ObjC-Photo-Browser
+//
+//  Created by Maciej Serwicki on 3/16/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosVC : UICollectionViewController
+
+@end
--- instagramAPI-ObjC-Photo-Browser/AppDelegate.m
+++ instagramAPI-ObjC-Photo-Browser/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "PhotosVC.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    UIWindow *window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [window setRootViewController:[[PhotosVC alloc] init]];
+    [window makeKeyAndVisible];
+    [self setWindow:window];
+    
+    return YES;
+    
     return YES;
 }
 
