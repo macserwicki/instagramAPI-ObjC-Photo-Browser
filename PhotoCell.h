51232c9343dfbea56b28dca515a156a82fc75f4b
Maciej Serwicki <macserwicki@gmail.com>
2016-03-19 10:14:27 -0500

    Custom Cell Added

---
+++ PhotoCell.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.h
+//  instagramAPI-ObjC-Photo-Browser
+//
+//  Created by Maciej Serwicki on 3/19/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCell : UICollectionViewCell
+
+@end
---
+++ PhotoCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.m
+//  instagramAPI-ObjC-Photo-Browser
+//
+//  Created by Maciej Serwicki on 3/19/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+@end
